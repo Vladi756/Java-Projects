478d27dd4c2bd2c22bc6a4707d0b74212b078a05
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-12 22:14:21 +0100

    Main File
    This main file calls creates a new object, everything is then done behind the scenes in the other file. Finally, the results are outputted by calling the "showInfo()" method.

--- Email Application/EmailApplication.java
+++ Email Application/EmailApplication.java
@@ -1,9 +1,9 @@
 package email.application;
 
-public class EmailApplication {
+public class EmailApplication {                         
     public static void main(String[] args) {
-        Email em1 = new Email("John", "Doe");
+        Email em1 = new Email("John", "Doe");           // Creates new object "em1" with class Email
         
-        System.out.println(em1.showInfo());
+        System.out.println(em1.showInfo());             // Prints out the results of the show info method. 
     }
 }
