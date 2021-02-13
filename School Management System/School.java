27bd7405d065fc4caa02328a070d0c5bd5cbd2fc
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-13 20:36:46 +0100

    Schol

---
+++ School Management System/School.java
@@ -0,0 +1,57 @@
+package school_management_system;
+import java.util.List;
+
+public class School {
+    
+    /*
+    Implementing an array list is like making a dynamic array.
+    */
+    
+    private List<Teachers> teachers;
+    private List<Students> students;
+    private static int totalMoneyEarned;
+    private static int totalMoneySpent;
+
+    public School(List<Teachers> teachers, List<Students> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    // Getters 
+    
+    public List<Teachers> getTeachers() {
+        return teachers;
+    }
+
+    public List<Students> getStudents() {
+        return students;
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    // Setters 
+    
+    public void addTeacher(Teachers teacher) {
+        teachers.add(teacher);
+    }
+
+    public void addStudent(Students student) {
+        students.add(student);
+    }
+
+    public static void updateTotalMoneyEarned(int MoneyEarned) {
+        totalMoneyEarned += MoneyEarned;
+    }
+
+    public static void updateTotalMoneySpent(int MoneySpent) {
+        totalMoneyEarned -= MoneySpent;
+    }      
+}
